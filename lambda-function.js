4335eff9c28810b7c1631d82c6a64cbde1c854fb
anirudh-92 <100350582+anirudh-92@users.noreply.github.com>
2022-08-15 10:48:49 +0100

    Update lambda-function.js

--- lambda-function.js
+++ lambda-function.js
@@ -1,67 +1,53 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
 exports.handler = async (event, context) => {
-  let body;
-  let statusCode = 200;
-  const headers = {
-    "Content-Type": "application/json"
-  };
+    //console.log('Received event:', JSON.stringify(event, null, 2));
 
-  try {
-    switch (event.routeKey) {
-      case "DELETE /items/{id}":
-        await dynamo
-          .delete({
-            TableName: "exercise-items",
-            Key: {
-              id: event.pathParameters.id
-            }
-          })
-          .promise();
-        body = `Deleted item ${event.pathParameters.id}`;
-        break;
-      case "GET /items/{id}":
-        body = await dynamo
-          .get({
-            TableName: "exercise-items",
-            Key: {
-              id: event.pathParameters.id
-            }
-          })
-          .promise();
-        break;
-      case "GET /items":
-        body = await dynamo.scan({ TableName: "exercise-items" }).promise();
-        break;
-      case "PUT /items":
-        let requestJSON = JSON.parse(event.body);
-        await dynamo
-          .put({
-            TableName: "exercise-items",
-            Item: {
-              id: requestJSON.id,
-              price: requestJSON.price,
-              name: requestJSON.name
-            }
-          })
-          .promise();
-        body = `Put item ${requestJSON.id}`;
-        break;
-      default:
-        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    try {
+        switch (event.httpMethod) {
+            case 'DELETE':
+                body = await dynamo.delete(JSON.parse(event.body)).promise();
+                break;
+            case 'GET':
+                body = await dynamo.scan({ TableName: event.queryStringParameters.TableName }).promise();
+                break;
+            case 'POST':
+                body = await dynamo.put(JSON.parse(event.body)).promise();
+                break;
+            case 'PUT':
+                body = await dynamo.update(JSON.parse(event.body)).promise();
+                break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
     }
-  } catch (err) {
-    statusCode = 400;
-    body = err.message;
-  } finally {
-    body = JSON.stringify(body);
-  }
 
-  return {
-    statusCode,
-    body,
-    headers
-  };
+    return {
+        statusCode,
+        body,
+        headers,
+    };
 };
